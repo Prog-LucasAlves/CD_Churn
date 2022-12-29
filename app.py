505eefe015472521ea814bd4c1d316019beeaba3
Prog-LucasAlves <lucasalves_taz@hotmail.com>
2022-12-29 17:43:20 -0300

    CD

--- app.py
+++ app.py
@@ -1,8 +1,5 @@
 import pandas as pd
 import streamlit as st
-from streamlit_disqus import st_disqus
-
-st_disqus("streamlit-disqus-demo")
 
 st.write("""
         Previsão de Churn\n
@@ -14,40 +11,38 @@ st.sidebar.text('Selecione os Dados dos Cliente:')
 
 st.write("-----------------------------------------")
 
-@st.cache()
-def getInfoData():
 
-    st.sidebar.header('Informações dos Clientes:')
-    a = 0
-    st.sidebar.text('Idoso(a):')
-    nSeniorCitizen = st.sidebar.button('Não', key=1)
-    ySeniorCitizen = st.sidebar.button('Sim', key=2)
+st.sidebar.header('Informações dos Clientes:')
+a = 0
+st.sidebar.text('Idoso(a):')
+nSeniorCitizen = st.sidebar.button('Não', key=1)
+ySeniorCitizen = st.sidebar.button('Sim', key=2)
     
-    if nSeniorCitizen is True:
-        a = 0
-    if ySeniorCitizen is True:
-        a = 1
+if nSeniorCitizen is True:
+    a = 0
+if ySeniorCitizen is True:
+    a = 1
         
-    b = 0
-    st.sidebar.text('Idoso(a):')
-    nDependents = st.sidebar.button('Não', key=3)
-    yDependents = st.sidebar.button('Sim', key=4)
+b = 0
+st.sidebar.text('Idoso(a):')
+nDependents = st.sidebar.button('Não', key=3)
+yDependents = st.sidebar.button('Sim', key=4)
 
-    if nDependents is True:
-        b = 0
-    if yDependents is True:
-        b = 1
+if nDependents is True:
+    b = 0
+if yDependents is True:
+    b = 1
 
     user_data = {
         'Idoso': a,
         'Tem Dependente': b,
     }
 
-    features = pd.DataFrame(user_data, index=[0])
-    return features
+features = pd.DataFrame(user_data, index=[0])
+    
 
 
-userInputData = getInfoData()                    
+userInputData = features                 
 
 st.subheader('Dados Do Cliente:')
 st.write(userInputData)
