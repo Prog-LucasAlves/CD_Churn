b0c7759443c01422dade831c8fdce14c0ce58206
Prog-LucasAlves <lucasalves_taz@hotmail.com>
2022-12-29 19:15:58 -0300

    CD

--- app.py
+++ app.py
@@ -3,11 +3,9 @@ import streamlit as st
 import time
 
 
-
 @st.cache(suppress_st_warning=True)
 def expensive_computation(a, b):
     st.write("Cache miss: expensive_computation(", a, ",", b, ") ran")
-
 
 st.write("""
         Previsão de Churn\n
@@ -19,11 +17,10 @@ st.sidebar.text('Selecione os Dados dos Cliente:')
 
 st.write("-----------------------------------------")
 
-
 st.sidebar.header('Informações dos Clientes:')
 a = 0
 SeniorCitizen = st.sidebar.selectbox('Cliente e Idoso(a):',
-                                    ('Não', 'Sim'))
+                                     ('Não', 'Sim'))
 st.sidebar.write('You selected:', SeniorCitizen)
 
 if SeniorCitizen == 'Não':
@@ -33,7 +30,7 @@ if SeniorCitizen == 'Sim':
 
 b = 0
 Dependents = st.sidebar.selectbox('Cliente tem Dependente:',
-                                    ('Não', 'Sim'))
+                                 ('Não', 'Sim'))
 st.sidebar.write('You selected:', Dependents)
 
 if Dependents == 'Não':
@@ -41,20 +38,18 @@ if Dependents == 'Não':
 if Dependents == 'Sim':
     b = 1
 
-tenure = st.sidebar.number_input('Quantos Meses o Cliente esta na Empresa:')
-st.write('You selected:', tenure)
+tenure = st.sidebar.number_input('Quantos Meses o Cliente esta na Empresa:', value=int)
+st.sidebar.write('You selected:', tenure)
 
 user_data = {
-        'Idoso': a,
-        'Tem Dependente': b,
-        'Meses como Cliente': tenure
-    }
+    'Idoso': a,
+    'Tem Dependente': b,
+    'Meses como Cliente': tenure
+            }
 
 features = pd.DataFrame(user_data, index=[0])
 
-userInputData = features                 
+userInputData = features         
 
 st.subheader('Dados Do Cliente:')
 st.write(features)
-
-
