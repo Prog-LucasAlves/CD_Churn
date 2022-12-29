78d1cf3f0a6dc2478a1b217660d9533d3ceb2409
Prog-LucasAlves <lucasalves_taz@hotmail.com>
2022-12-29 19:31:18 -0300

    CD

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import streamlit as st
 def expensive_computation(a, b):
     st.write("Cache miss: expensive_computation(", a, ",", b, ") ran")
 
+
 st.write("""
         Previsão de Churn\n
         App que utiliza Machine Learnig para prever Churn de clintes.\n
@@ -29,7 +30,7 @@ if SeniorCitizen == 'Sim':
 
 b = 0
 Dependents = st.sidebar.selectbox('Cliente tem Dependente:',
-                                 ('Não', 'Sim'))
+                                  ('Não', 'Sim'))
 st.sidebar.write('You selected:', Dependents)
 
 if Dependents == 'Não':
@@ -37,14 +38,16 @@ if Dependents == 'Não':
 if Dependents == 'Sim':
     b = 1
 
-tenure = st.sidebar.number_input('Quantos Meses o Cliente esta na Empresa:', step=0, max_value=75)
+tenure = st.sidebar.number_input('Quantos Meses o Cliente esta na Empresa:', 
+                                 step=0, 
+                                 max_value=75)
 st.sidebar.write('You selected:', tenure)
 
 user_data = {
     'Idoso(a)': a,
     'Tem Dependente': b,
     'Meses como Cliente': tenure
-            }
+    }
 
 user_info = {
     'Idoso(a)': SeniorCitizen,
@@ -55,8 +58,8 @@ user_info = {
 features_data = pd.DataFrame(user_data, index=[0])
 features_info = pd.DataFrame(user_info, index=[0])
 
-st.subheader('Dados Do Cliente Para Modelo:')
+st.subheader('⚙️ Dados Do Cliente Para Modelo:')
 st.write(features_data)
 
-st.subheader('Dados Do Cliente:')
+st.subheader('👪 Dados Do Cliente:')
 st.write(features_info)
