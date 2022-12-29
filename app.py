daff78f652897eb9d9c034ec16cda4bed853fc3c
Prog-LucasAlves <lucasalves_taz@hotmail.com>
2022-12-29 17:24:24 -0300

    CD

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ st.sidebar.text('Selecione os Dados dos Cliente:')
 
 st.write("-----------------------------------------")
 
-@st.cache
+@st.cache(suppress_st_warning=True)
 def getInfoData():
 
     a = 0
